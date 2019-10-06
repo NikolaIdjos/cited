d91ac01a6b749a326a8a9a966e8dd81136e2a97b
idjos <nikolaidjos@gmail.com>
2019-10-06 17:05:38 +0200

    subscribers

---
+++ database/seeds/SubscribersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Subscriber;
+
+class SubscribersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $status = ['ACTIVE', 'INACTIVE'];
+        $type = ['PAID', 'UNPAID'];
+        for ($i = 0; $i <= 10; $i++) {
+           Subscriber::create([
+                'email' => $faker->email(),
+                'status' => $status[rand(0,1)],
+                'type' => $type[rand(0,1)],
+            ]);
+        }
+    }
+}
